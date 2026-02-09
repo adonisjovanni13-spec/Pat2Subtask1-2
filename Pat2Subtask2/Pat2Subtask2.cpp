a6427c98006db92ea9ca7cb799618ef6f955dd00
ldickens <ldickens@ROBOTICSA2>
2026-02-09 13:00:56 +0200

    Added Lathitha & Njokweni

--- Pat2Subtask2/Pat2Subtask2.cpp
+++ Pat2Subtask2/Pat2Subtask2.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Hello Bahlali" << endl;
     cout << "Unam is bunking class again" << endl;
     cout << "I like Thuli and Thando" << endl;
+    cout << "Njokweni Wants Gihub, while Lathitha wants to satisfy her docs" << endl;
     return 0;
 }
 
